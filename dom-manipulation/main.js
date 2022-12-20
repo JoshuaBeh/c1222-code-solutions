0939217816628bb8fce48987bd39eb79f2b37e2e
Joshua Beh <joshbeh88@gmail.com>
2022-12-20 00:02:41 +0000

    Updated

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -4,16 +4,16 @@ var $clickCount = document.querySelector('.click-count');
 
 function handleClick(event) {
   clicked++;
-  $clickCount.textContent = clicked;
+  $clickCount.textContent = 'Clicks:' + ' ' + clicked;
   if (clicked < 4) {
     $hotButton.className = 'hot-button cold';
-  } else if (clicked === 4 || clicked < 7) {
+  } else if (clicked < 7) {
     $hotButton.className = 'hot-button cool';
-  } else if (clicked === 7 || clicked < 10) {
+  } else if (clicked < 10) {
     $hotButton.className = 'hot-button tepid';
-  } else if (clicked === 10 || clicked < 13) {
+  } else if (clicked < 13) {
     $hotButton.className = 'hot-button warm';
-  } else if (clicked === 13 || clicked < 16) {
+  } else if (clicked < 16) {
     $hotButton.className = 'hot-button hot';
   } else {
     $hotButton.className = 'hot-button nuclear';
