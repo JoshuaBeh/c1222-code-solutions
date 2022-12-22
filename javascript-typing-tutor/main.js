9359da91794b5c0b944191ba317606d3b7d5e057
Joshua Beh <joshbeh88@gmail.com>
2022-12-22 23:11:53 +0000

    updating for main pc

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,13 +1,17 @@
 var $span = document.querySelectorAll('h1 > span');
-var $accuracy = document.querySelector('h2');
+// eslint-disable-next-line no-unused-vars
+var $accuracyLabel = document.querySelector('h2');
+var $accuracy = document.querySelector('h2 > span');
 var u = 1;
 var i = 0;
 var counter = 0;
 function keydownHandler(event) {
   counter++;
   if (i === $span.length - 1) {
+    // show accuracy header
     var accuracy = Math.round((30 / counter) * 100);
-    $accuracy.textContent = 'Accuracy:' + ' ' + accuracy + '%';
+    $accuracy.textContent = accuracy + '%';
+    // set class on span to appropriate for the score
     $span[i].classList.remove('border');
     $span[i].classList.remove('red');
   }
