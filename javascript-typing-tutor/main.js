1a9ef347c0bdce535531045620a2c116c649ad67
Joshua Beh <joshbeh88@gmail.com>
2022-12-22 04:09:03 +0000

    Completed javascript-typing-tutor exercise.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,7 +1,16 @@
-var $span = document.querySelectorAll('span');
+var $span = document.querySelectorAll('h1 > span');
+var $accuracy = document.querySelector('h2');
 var u = 1;
 var i = 0;
+var counter = 0;
 function keydownHandler(event) {
+  counter++;
+  if (i === $span.length - 1) {
+    var accuracy = Math.round((30 / counter) * 100);
+    $accuracy.textContent = 'Accuracy:' + ' ' + accuracy + '%';
+    $span[i].classList.remove('border');
+    $span[i].classList.remove('red');
+  }
   if ($span[i].textContent === event.key) {
     $span[i].classList.add('green');
     $span[u].classList.add('border');
