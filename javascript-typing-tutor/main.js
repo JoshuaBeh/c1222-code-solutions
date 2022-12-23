8f4cca8ad95d2a00c9b9716d818573a413c06c18
Joshua Beh <joshbeh88@gmail.com>
2022-12-23 00:27:40 +0000

    Added colors to accuracy number and text for <10%

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,19 +1,26 @@
 var $span = document.querySelectorAll('h1 > span');
-// eslint-disable-next-line no-unused-vars
 var $accuracyLabel = document.querySelector('h2');
 var $accuracy = document.querySelector('h2 > span');
+var $how = document.querySelector('#how');
 var u = 1;
 var i = 0;
 var counter = 0;
 function keydownHandler(event) {
   counter++;
   if (i === $span.length - 1) {
-    // show accuracy header
+    $accuracyLabel.classList.remove('hidden');
     var accuracy = Math.round((30 / counter) * 100);
     $accuracy.textContent = accuracy + '%';
-    // set class on span to appropriate for the score
     $span[i].classList.remove('border');
     $span[i].classList.remove('red');
+    if (accuracy < 80 && accuracy > 40) {
+      $accuracy.className = 'yellow';
+    } else if (accuracy < 40 && accuracy > 11) {
+      $accuracy.className = 'red-2';
+    } else if (accuracy < 11) {
+      $accuracy.className = 'how-color';
+      $how.textContent = '\u2190 How did this happen?';
+    }
   }
   if ($span[i].textContent === event.key) {
     $span[i].classList.add('green');
