9e7719f35fc94ea7e9ae16d9f2d05a10dc4139b0
Joshua Beh <joshbeh88@gmail.com>
2023-01-29 01:16:30 +0000

    Fully fixed write error handing

--- express-notes-api/write.js
+++ express-notes-api/write.js
@@ -1,10 +1,16 @@
 const fs = require('fs');
 
 function write(path, data) {
-  fs.writeFile(path, JSON.stringify(data, null, 2), (err) => {
+  fs.readFile(path, 'utf-8', (err) => {
     if (err) {
-      console.error('Error when writing', err);
+      console.error(err);
+      return;
     }
+    fs.writeFile(path, JSON.stringify(data, null, 2), (err) => {
+      if (err) {
+        console.error('Error when writing', err);
+      }
+    });
   });
 }
 module.exports = write;
