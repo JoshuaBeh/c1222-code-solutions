30143abb83c08b52c799a64a6483cf1847fc8cd6
Joshua Beh <joshbeh88@gmail.com>
2023-02-07 04:09:11 +0000

    Completed react-stopwatch-component exercise.

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -3,31 +3,43 @@ import React, { useState } from 'react';
 export default function StopWatch() {
   const switchClasses = {
     play: {
-      icon: 'fa-play'
+      icon: 'fa-play',
+      playColor: 'green'
     },
     pause: {
-      icon: 'fa-pause'
+      icon: 'fa-pause',
+      playColor: 'red'
     },
     count: 0
   };
+
+  let [start, setStart] = useState(null);
   const [toggle, setToggle] = useState(false);
-  const [time, setTime] = useState(0);
+  let [time, setTime] = useState(0);
   function handlePlayClick() {
+    clearInterval(start);
     const currentToggle = !toggle;
     setToggle(currentToggle);
     if (currentToggle) {
-      setInterval(() => {
-        setTime(time + 1);
+      start = setInterval(() => {
+        setTime(time++);
       }, 1000);
     }
+    setStart(start);
+  }
+
+  function handleButtonClick() {
+    setTime(0);
+    clearInterval(start);
+    setToggle(false);
   }
 
   const classes = switchClasses[toggle ? 'pause' : 'play'];
   return (
     <div className='row'>
-      <button>{time}</button>
+      <button onClick={handleButtonClick}>{time}</button>
       <div>
-        <i onClick={handlePlayClick} className={`fa-solid ${classes.icon}`}></i>
+        <i onClick={handlePlayClick} className={`fa-solid ${classes.icon} ${classes.playColor}`}></i>
       </div>
     </div>
   );
