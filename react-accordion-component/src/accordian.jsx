8a51ae25952410145c059c4147e306589a495304
Joshua Beh <joshbeh88@gmail.com>
2023-02-09 17:20:11 +0000

    Made accordion alot better

--- react-accordion-component/src/accordian.jsx
+++ react-accordion-component/src/accordian.jsx
@@ -1,34 +1,57 @@
 import React, { useState } from 'react';
 
 export default function Accordian({ items }) {
-  const [isOpen, setIsOpen] = useState('');
 
   return (
     <div className='row'>
-      <ListItems items={items} isOpen={isOpen} setIsOpen={setIsOpen} />
+      {
+        items.map((item) => <ListItem key={item.language} item={item} />)
+      }
     </div>
   );
 }
 
-function randomColor() {
-  const classes = {
-    width: '25%',
-    backgroundColor: 'rgb(' + Math.floor(Math.random() * 255) +
-      ',' + Math.floor(Math.random() * 255) + ',' +
-      Math.floor(Math.random() * 255) + ')'
-  };
-  return classes;
-}
-
-function ListItems({ items, isOpen, onClick, setIsOpen }) {
+function ListItem({ item }) {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <>
-      {items.map((item) => (
-        <div key={item.language} style={randomColor()}>
-          <h1 onClick={() => isOpen === item.language ? setIsOpen('') : setIsOpen(item.language)}>{item.language}</h1>
-          <p className={`${isOpen === item.language ? '' : 'hidden'}`}>{item.description}</p>
-        </div>
-      ))}
-    </>
+    <div key={item.language} className="item">
+      <h1 onClick={() => setIsOpen(!isOpen)}>{item.language}</h1>
+      <p className={`${isOpen ? '' : 'hidden'}`}>{item.description}</p>
+    </div>
   );
 }
+
+// Below works as well.
+
+// export default function Accordian({ items }) {
+//   const [isOpen, setIsOpen] = useState('');
+
+//   return (
+//     <div className='row'>
+//       <ListItems items={items} isOpen={isOpen} setIsOpen={setIsOpen} />
+//     </div>
+//   );
+// }
+
+// function randomColor() {
+//   const classes = {
+//     width: '25%',
+//     backgroundColor: 'rgb(' + Math.floor(Math.random() * 255) +
+//       ',' + Math.floor(Math.random() * 255) + ',' +
+//       Math.floor(Math.random() * 255) + ')'
+//   };
+//   return classes;
+// }
+
+// function ListItems({ items, isOpen, onClick, setIsOpen }) {
+//   return (
+//     <>
+//       {items.map((item) => (
+//         <div key={item.language} style={randomColor()}>
+//           <h1 onClick={() => isOpen === item.language ? setIsOpen('') : setIsOpen(item.language)}>{item.language}</h1>
+//           <p className={`${isOpen === item.language ? '' : 'hidden'}`}>{item.description}</p>
+//         </div>
+//       ))}
+//     </>
+//   );
+// }
