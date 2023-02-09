0fbe049d0183481269d50a1fbd9a84f3c11f3543
Joshua Beh <joshbeh88@gmail.com>
2023-02-09 06:46:37 +0000

    Completed react-accordian-component exercise.

--- react-accordion-component/src/accordian.jsx
+++ react-accordion-component/src/accordian.jsx
@@ -1,37 +1,34 @@
 import React, { useState } from 'react';
 
-export default function Accordian() {
-  const items = [
-    {
-      language: 'Hypertext Markup Language',
-      description: 'The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.'
-    },
-    {
-      language: 'Cascading Style Sheets',
-      description: 'Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language such as HTML or XML (including XML dialects such as SVG, MathML or XHTML).[1] CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.'
-    },
-    {
-      language: 'JavaScript',
-      description: 'JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. As of 2022, 98% of websites use JavaScript on the client side for webpage behavior, often incorporating third-party libraries. All major web browsers have a dedicated JavaScript engine to execute the code on users devices.'
-    }
-  ];
-
-  // eslint-disable-next-line no-unused-vars
-  const [isOpen, setIsOpen] = useState(false);
-  function itemHandler(input) {
-
-  }
+export default function Accordian({ items }) {
+  const [isOpen, setIsOpen] = useState('');
 
   return (
     <div className='row'>
-      {
-        items.map((item) => (
-          <div key={item.language} className="item">
-            <h1 onClick={() => itemHandler(item.language)}>{item.language}</h1>
-            <p className={item.language.split(' ').join('-').toLowerCase()}>{item.description}</p>
-          </div>
-        ))
-      }
+      <ListItems items={items} isOpen={isOpen} setIsOpen={setIsOpen} />
     </div>
+  );
+}
+
+function randomColor() {
+  const classes = {
+    width: '25%',
+    backgroundColor: 'rgb(' + Math.floor(Math.random() * 255) +
+      ',' + Math.floor(Math.random() * 255) + ',' +
+      Math.floor(Math.random() * 255) + ')'
+  };
+  return classes;
+}
+
+function ListItems({ items, isOpen, onClick, setIsOpen }) {
+  return (
+    <>
+      {items.map((item) => (
+        <div key={item.language} style={randomColor()}>
+          <h1 onClick={() => isOpen === item.language ? setIsOpen('') : setIsOpen(item.language)}>{item.language}</h1>
+          <p className={`${isOpen === item.language ? '' : 'hidden'} ${item.language.split(' ').join('-').toLowerCase()}`}>{item.description}</p>
+        </div>
+      ))}
+    </>
   );
 }
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -2,6 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Accordian from './accordian';
 
+const items = [
+  {
+    language: 'Hypertext Markup Language',
+    description: 'The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.'
+  },
+  {
+    language: 'Cascading Style Sheets',
+    description: 'Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language such as HTML or XML (including XML dialects such as SVG, MathML or XHTML).[1] CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.'
+  },
+  {
+    language: 'JavaScript',
+    description: 'JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. As of 2022, 98% of websites use JavaScript on the client side for webpage behavior, often incorporating third-party libraries. All major web browsers have a dedicated JavaScript engine to execute the code on users devices.'
+  }
+];
+
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
-root.render(<Accordian />);
+root.render(<Accordian items={items}/>);
