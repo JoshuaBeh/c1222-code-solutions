36422be8bdfe10cc1b3b99c8f7208ad64607bba7
Joshua Beh <joshbeh88@gmail.com>
2023-02-10 01:14:06 +0000

    Basic functionality done.

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -1,7 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export default function Carousel() {
+export default function Carousel({ images }) {
+  const [curImage, setCurImage] = useState(0);
+  const length = images.length;
+
+  function nextSlide() {
+    setCurImage(curImage === length - 1 ? 0 : curImage + 1);
+  }
+
+  function prevSlide() {
+    setCurImage(curImage === 0 ? length - 1 : curImage - 1);
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurImage(curImage === length - 1 ? 0 : curImage + 1);
+    }, 3000);
+    return () => clearInterval(interval);
+  });
+
+  const imgHolder = {
+    transform: 'translateX(' + (-curImage * 100) + '%)',
+    display: 'flex'
+  };
+
   return (
-    <div></div>
+    <div className='row'>
+      <div className='col-half'>
+        <div className='container'>
+          <i className='fa-solid fa-chevron-left' id='left-arrow' onClick={prevSlide}></i>
+          <i className='fa-solid fa-chevron-right' id='right-arrow' onClick={nextSlide}></i>
+          <div className='img-holder' style={imgHolder}>
+            {
+              images.map((image) => <CarouselImage key={image.id} image={image} />)
+            }
+          </div>
+        </div>
+        <div className='button-wrapper'>
+          {
+            images.map((image) => <CarouselButtons key={image.id} image={image} />)
+          }
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function CarouselImage({ image }) {
+  const alt = image.src.split('/')[2].split('.')[0];
+  return (
+    <img src={image.src} alt={alt} />
+  );
+}
+
+function CarouselButtons({ image }) {
+  return (
+    <button id={image.id}></button>
   );
 }
--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -2,6 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Carousel from './carousel';
 
+const images = [
+  {
+    id: 0,
+    src: '../images/bulb.png'
+  },
+  {
+    id: 1,
+    src: '../images/char.png'
+  },
+  {
+    id: 2,
+    src: '../images/jiggly.png'
+  },
+  {
+    id: 3,
+    src: '../images/pika.png'
+  },
+  {
+    id: 4,
+    src: '../images/squirtle.png'
+  }
+];
+
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
-root.render(<Carousel />);
+root.render(<Carousel images={images}/>);
