4906e9621961ddbd8644db2fa3d0a8ec68bc9087
Joshua Beh <joshbeh88@gmail.com>
2023-03-07 17:40:09 +0000

    Completed data-structure-queues exercise.

--- data-structures-queues/take-2nd.js
+++ data-structures-queues/take-2nd.js
@@ -1,14 +1,11 @@
 /* exported take2nd */
 
 function take2nd(queue) {
-  // if (queue.peek() === undefined) {
-  //   return undefined;
-  // }
   const front = queue.peek();
   if (front === undefined) {
     return;
   }
-  queue.dequeue();
   queue.enqueue(front);
-  return queue.peek();
+  queue.dequeue();
+  return queue.dequeue();
 }
